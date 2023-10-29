1274dd993f27952ea665486acf8ad98b73e45a81
반영환 <lopahn2@gmail.com>
2023-10-29 23:23:28 +0900

    init

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,51 @@
-import express from 'express';
-import sqlCon from '../db/sqlCon.js'
-const conn = sqlCon();
+import express from "express";
+import crypto from "crypto";
+import axios from "axios";
 const router = express.Router();
 
+let jwt = "";
 
-/* GET home page. */
-router.get('/', async function(req, res, next) {
-  const DBSearchResult = await conn.execute('SELECT * FROM user_auth_info');
-  console.log(DBSearchResult[0]);
-  return res.send(DBSearchResult[0]); 
+router.post("/auth", async function (req, res, next) {
+  // const teamName = req.teamName;
+  const postUrl = "http://118.67.134.151/hackathon/api/auth";
+  console.log(req.body.teamName);
+  const message = req.body.teamName;
+  const secretKey = "NHVzLwHNeIXuNIiXerePIGVj5kzJmJYA";
+
+  const hmac = crypto.createHmac("sha256", secretKey);
+  const authHash = hmac.update(message).digest("base64");
+
+  const data = {
+    message,
+    authHash,
+  };
+
+  const result = await axios.post(postUrl, data, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  jwt = result.data.data;
+  return res.json(result.data);
+});
+
+router.post("/save", async function (req, res, next) {
+  // const teamName = req.teamName;
+  const postUrl = "http://118.67.134.151/hackathon/api/save";
+  const teamName = req.body.teamName;
+
+  const data = {
+    teamName,
+  };
+  console.log(jwt);
+
+  const result = await axios.post(postUrl, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${jwt}`,
+    },
+  });
+  return res.json(result.data);
 });
 
 export default router;
